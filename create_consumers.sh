bf4e8f03efdc78469d705cea6cc9f104df0f4fe3
BENYEMNA Hamza <96919179+BENYEMNA-Hamza-DIA@users.noreply.github.com>
2025-06-19 09:44:15 +0200

    Update create_consumers.sh

--- create_consumers.sh
+++ create_consumers.sh
@@ -6,6 +6,8 @@ CONSUMERS_GROUPS["topic2"]=2
 CONSUMERS_GROUPS["topic3"]=3
 
 BOOTSTRAP_SERVER="kafka-lb:9092"
+SESSION_TIMEOUT="45000"           # 45 secondes
+MAX_POLL_INTERVAL="120000"        # 2 minutes
 
 echo "👥 [INFO] Création des Consumer Groups..."
 for topic in "${!CONSUMERS_GROUPS[@]}"; do
@@ -14,6 +16,12 @@ for topic in "${!CONSUMERS_GROUPS[@]}"; do
 
     for (( i=1; i<=NUM_CONSUMERS; i++ )); do
         echo "🎧 [INFO] Démarrage du Consumer $i dans $GROUP_NAME..."
-        docker-compose exec -T kafka1 kafka-console-consumer --topic $topic --group $GROUP_NAME --bootstrap-server $BOOTSTRAP_SERVER > "consumer_${GROUP_NAME}_${i}.log" 2>&1 &
+        docker-compose exec -T kafka1 kafka-console-consumer \
+            --topic "$topic" \
+            --group "$GROUP_NAME" \
+            --bootstrap-server "$BOOTSTRAP_SERVER" \
+            --consumer-property session.timeout.ms=$SESSION_TIMEOUT \
+            --consumer-property max.poll.interval.ms=$MAX_POLL_INTERVAL \
+            > "consumer_${GROUP_NAME}_${i}.log" 2>&1 &
     done
 done
