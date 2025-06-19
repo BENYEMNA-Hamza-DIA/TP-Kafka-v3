9700d559532b9c5ccff3b7487cc10c24f5a5fc36
BENYEMNA Hamza <96919179+BENYEMNA-Hamza-DIA@users.noreply.github.com>
2025-06-19 10:48:11 +0200

    Update run_pipeline_kafka_v1.sh

--- run_pipeline_kafka_v1.sh
+++ run_pipeline_kafka_v1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo bash start_kafka.sh
-sleep 7
+sleep 5
 
 sudo bash create_topics.sh
-sleep 7
+sleep 5
 
 sudo bash create_consumers.sh
-sleep 7
+sleep 5
 
 sudo bash produce_messages_auto_v1.sh
 sleep 10
